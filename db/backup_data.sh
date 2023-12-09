7afa6785b431f93a470be313b5d5a5a635c41326
Luan E <luan.erazo@skyrocketlab.space>
2023-12-09 09:12:46 -0500

    removed dangerous files

--- db/backup_data.sh
+++
@@ -1,4 +0,0 @@
-# Update line 3 with your database name, user name, and password
-echo 'Exporting database from MySQL...';
-mysqldump --user='u904348789_trbKy' --password='zxvdfimRBw' u904348789_kBmaU > ./data.sql
-echo '---------------> Export complete!! <---------------';
